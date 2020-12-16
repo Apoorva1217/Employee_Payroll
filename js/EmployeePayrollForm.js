e6d0aff8a436404cf051f6e8e586571e90460bab
Apoorva1217 <apoorvarasal12@gmail.com>
2020-12-16 21:59:29 +0530

    [Apoorva] Added : Ability to Set Event Listener on Salary Range to display appropriate value

---
+++ js/EmployeePayrollForm.js
@@ -0,0 +1,6 @@
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function () {
+    output.textContent = salary.value;
+});
