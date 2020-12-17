55d8d98596fc3189327c1227efaf75ace677f1de
Apoorva1217 <apoorvarasal12@gmail.com>
2020-12-17 14:32:40 +0530

    [Apoorva] Added : On Form Submit populate the Employee Payroll Data Object

---
+++ js/EmployeePayroll.js
@@ -0,0 +1,53 @@
+class EmployeePayroll {
+    get id() { return this._id }
+    set id(id) {
+        this._id = id;
+    }
+
+    get name() { return this._name; }
+    set name(name) {
+        this._name = name;
+    }
+
+    get profileImg() { return this._profileImg; }
+    set profileImg(profileImg) {
+        this._profileImg = profileImg;
+    }
+
+    get gender() { return this._gender; }
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() { return this._department; }
+    set department(department) {
+        this._department = department;
+    }
+
+    get salary() { return this._salary; }
+    set salary(salary) {
+        this._salary = salary;
+    }
+
+    get startDate() { return this._startDate; }
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
+
+    get note() { return this._note; }
+    set note(note) {
+        this._note = note;
+    }
+
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const date = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "name = " + this.name +
+            "\n gender = " + this.gender +
+            "\n profile = " + this.profileImg +
+            "\n department = " + this.department +
+            "\n salary = " + this.salary +
+            "\n startDate = " + date +
+            "\n note = " + this.note;
+    }
+}
--- js/EmployeePayrollForm.js
+++ js/EmployeePayrollForm.js
@@ -4,3 +4,41 @@ output.textContent = salary.value;
 salary.addEventListener('input', function () {
     output.textContent = salary.value;
 });
+
+function save() {
+    try {
+        employeePayroll = createEmployeePayroll();
+    }
+    catch (message) {
+        return message;
+    }
+}
+
+function createEmployeePayroll() {
+    let employeePayroll = new EmployeePayroll();
+    employeePayroll.name = getInputValueById('#name');
+    employeePayroll.profileImg = getSelectedValues('[name = profileImg]').pop();
+    employeePayroll.gender = getSelectedValues('[name = gender]').pop();
+    employeePayroll.department = getSelectedValues('[name = department]');
+    employeePayroll.salary = getInputValueById('#salary');
+    employeePayroll.note = getInputValueById('#note');
+    let date = getInputValueById('#day') + "/" + getInputValueById('#month') + "/" + getInputValueById('#year');
+    employeePayroll.startDate = new Date(date);
+    alert(employeePayroll.toString());
+    return employeePayroll;
+}
+
+function getSelectedValues(propertyValue) {
+    let allItems = document.querySelector(propertyValue);
+    let selectedItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            selectedItems.push(item.value);
+    });
+    return selectedItems;
+}
+
+function getInputValueById(id) {
+    let value = document.querySelector(id).value;
+    return value;
+}
